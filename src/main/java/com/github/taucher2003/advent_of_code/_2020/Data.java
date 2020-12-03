4f8a9713f36b32a4465573505199dbd3a02e6724
Taucher <mc.taucher2003@gmail.com>
2020-12-03 16:53:34 +0100

    Changed File Path in classes to the new location

--- src/main/java/com/github/taucher2003/advent_of_code/_2020/Data.java
+++ src/main/java/com/github/taucher2003/advent_of_code/_2020/Data.java
@@ -2,5 +2,5 @@ package com.github.taucher2003.advent_of_code._2020;
 
 public class Data {
 
-    public static final String RESOURCE_PATH = "D:\\_Daten\\Development\\Java\\intellij-workspace\\advent-of-code\\src\\main\\resources\\";
+    public static final String RESOURCE_PATH_2020 = "D:\\_Daten\\Development\\Java\\intellij-workspace\\advent-of-code\\src\\main\\resources\\_2020\\";
 }
